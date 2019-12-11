803efb148eb4a7f411e657f9314fdec5a83c2f32
Damyan Peykov <dppeykov@gmail.com>
2019-12-11 11:16:20 +0100

    Assignment 1 done
    Videos 22 & 23, Section 2, Adding 2 alerts - success & warning as modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
+import { WarningAlertComponent } from "./WarningAlert/warningAlert.component";
+import { SuccessAlertComponent } from "./SuccessAlert/successAlert.component";
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
