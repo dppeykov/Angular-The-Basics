4ab222338177e17bbb2f79ad0583b0cde53a61dc
Damyan Peykov <dppeykov@gmail.com>
2019-12-11 10:58:57 +0100

    Adding styles

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
 
 // template: html -->> inline template html
+// styles: [] -->> we can use strings of styles directly without an external css file
 @Component({
   selector: "app-server",
-  templateUrl: "./server.component.html"
+  templateUrl: "./server.component.html",
+  styleUrls: ["./server.component.css"]
 })
 export class ServerComponent {}
