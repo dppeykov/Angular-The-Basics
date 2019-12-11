d93d330c942b90f7ecec1f75b5db8ddc7537a566
Damyan Peykov <dppeykov@gmail.com>
2019-12-11 11:46:34 +0100

    Testing

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,7 +4,11 @@ import { Component } from "@angular/core";
 // styles: [] -->> we can use strings of styles directly without an external css file
 @Component({
   selector: "app-server",
-  templateUrl: "./server.component.html",
-  styleUrls: ["./server.component.css"]
+  templateUrl: "./server.component.html"
+  //styleUrls: ["./server.component.css"]
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = "online/offline";
+
+}
